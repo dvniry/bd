b3a31355f08bf0bc86bac1f8eeebf7b004c8a4a7
dvniry <nikita.2005@mail.ru>
2024-12-02 23:20:50 +1000

    Добавление 2,3 и 5 лаб

---
+++ task_e.sql
@@ -0,0 +1,18 @@
+
+SELECT 
+    E.FullName AS EmployeeFullNames,
+    ROUND(SUM(P.Salary * S.SalaryPercentage)) AS SumSalary
+FROM Employees E
+JOIN Salaries S ON E.ID = S.EmployeeID
+JOIN Positions P ON S.PositionID = P.ID
+GROUP BY E.ID
+HAVING SUM(P.Salary * S.SalaryPercentage) < (
+    SELECT AVG(TotalSalary)
+    FROM (
+        SELECT SUM(P2.Salary * S2.SalaryPercentage) AS TotalSalary
+        FROM Salaries S2
+        JOIN Positions P2 ON S2.PositionID = P2.ID
+        GROUP BY S2.EmployeeID
+    )
+)
+ORDER BY SumSalary ASC;
